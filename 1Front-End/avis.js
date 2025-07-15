495def3fb0405a746b102453209da5b46c0a5a19
Bik237 <ngantybikele@gmail.com>
2025-07-15 12:57:41 +0100

    configuration du code avec eslint

--- 1Front-End/avis.js
+++ 1Front-End/avis.js
@@ -27,136 +27,136 @@
 //     return fab(n - 1) + fab(n - 2)
 // }
 // console.log(fib(5)) // 5
-/* global Chart */
+
 export function ajoutListenersAvis() {
-	const piecesElements = document.querySelectorAll(".fiches article button");
+    const piecesElements = document.querySelectorAll(".fiches article button");
 
-	// Vérifier si des éléments ont été trouvés
-	for (let i = 0; i < piecesElements.length; i++) {
-		piecesElements[i].addEventListener("click", async function (event) {
+    // Vérifier si des éléments ont été trouvés
+    for (let i = 0; i < piecesElements.length; i++) {
+        piecesElements[i].addEventListener("click", async function (event) {
 
-			const id = event.target.dataset.id
+            const id = event.target.dataset.id
 
-			let avisPieces = window.localStorage.getItem(`avis-pieces${id}`)
-			let avis
-			if (avisPieces) {
-				avis = JSON.parse(avisPieces)
-			} else {
-				const r = await fetch(`https://sitelesbonnespieces.onrender.com/pieces/${id}/avis`)
-				avis = await r.json()
-				window.localStorage.setItem(`avis-pieces${id}`, JSON.stringify(avis))
-			}
+            let avisPieces = window.localStorage.getItem(`avis-pieces${id}`)
+            let avis
+            if (avisPieces) {
+                avis = JSON.parse(avisPieces)
+            } else {
+                const r = await fetch(`https://sitelesbonnespieces.onrender.com/pieces/${id}/avis`)
+                avis = await r.json()
+                window.localStorage.setItem(`avis-pieces${id}`, JSON.stringify(avis))
+            }
 
-			piecesElements[i].disabled = true
-			const pieceElement = event.target.parentElement // Récupérer l'élément parent de l'élément bouton cliqué qui est l'article
-			affichagesdesAvis(pieceElement, avis)
-		});
-	}
+            piecesElements[i].disabled = true
+            const pieceElement = event.target.parentElement // Récupérer l'élément parent de l'élément bouton cliqué qui est l'article
+            affichagesdesAvis(pieceElement, avis)
+        });
+    }
 }
 function affichagesdesAvis(pieceElement, avis) {
-	//Créer un nouvel élément pour afficher les avis
-	const avisElement = document.createElement("p")
-	avis.forEach(item => {
-		avisElement.innerHTML += `<strong>${item.utilisateur}</strong>: ${item.commentaire}<br> 
+    //Créer un nouvel élément pour afficher les avis
+    const avisElement = document.createElement("p")
+    avis.forEach(item => {
+        avisElement.innerHTML += `<strong>${item.utilisateur}</strong>: ${item.commentaire}<br> 
         nobre d'etoiles: ${item.nbEtoiles}<br>`
-	})
-	avisElement.classList.add("avis")
-	pieceElement.appendChild(avisElement)
+    })
+    avisElement.classList.add("avis")
+    pieceElement.appendChild(avisElement)
 }
 
 
 export function envoyerAvis() {
-	const formulaireAvis = document.querySelector(".formulaire-avis")
-	formulaireAvis.addEventListener("submit", async (event) => {
-		event.preventDefault()
-		//recupere les donnnees du formulaire et stocke pour etre utiliser dans body 
-		const avis = {
-			pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-			utilisateur: event.target.querySelector("[name=utilisateur]").value,
-			commentaire: event.target.querySelector("[name=commentaire]").value,
-			nbEtoiles: parseInt(event.target.querySelector("[name=etoiles]").value)
-		}
+    const formulaireAvis = document.querySelector(".formulaire-avis")
+    formulaireAvis.addEventListener('submit', async (event) => {
+        event.preventDefault()
+        //recupere les donnnees du formulaire et stocke pour etre utiliser dans body 
+        const avis = {
+            pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+            utilisateur: event.target.querySelector("[name=utilisateur]").value,
+            commentaire: event.target.querySelector("[name=commentaire]").value,
+            nbEtoiles: parseInt(event.target.querySelector("[name=etoiles]").value)
+        }
 
-		const afficherAvis = JSON.stringify(avis)
-		await fetch("https://sitelesbonnespieces.onrender.com/avis", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: afficherAvis
-		})
-		event.target.reset()
-		//recupere dans le serveur tout les avis qui correspondent a pieceId ex: si pieceId = 3 recupere tous les avis de la piece 3
-		const r = await fetch(`https://sitelesbonnespieces.onrender.com/pieces/${avis.pieceId}/avis`);
-		const avisActualises = await r.json();
-		localStorage.setItem(`avis-pieces${avis.pieceId}`, JSON.stringify(avisActualises));//stocke les avis de cette piece dans mon localstorage ce qui permet la mise a jour
+        const afficherAvis = JSON.stringify(avis)
+        await fetch("https://sitelesbonnespieces.onrender.com/avis", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: afficherAvis
+        })
+        event.target.reset()
+        //recupere dans le serveur tout les avis qui correspondent a pieceId ex: si pieceId = 3 recupere tous les avis de la piece 3
+        const r = await fetch(`https://sitelesbonnespieces.onrender.com/pieces/${avis.pieceId}/avis`);
+        const avisActualises = await r.json();
+        localStorage.setItem(`avis-pieces${avis.pieceId}`, JSON.stringify(avisActualises));//stocke les avis de cette piece dans mon localstorage ce qui permet la mise a jour
 
-	})
+    })
 }
 
 
 export async function afficherGraphe() {
-	const avis = await fetch("https://sitelesbonnespieces.onrender.com/avis").then(avis => avis.json())
-	const nb_commentaire = [0, 0, 0, 0, 0]
-	for (let comentaire of avis) {
-		nb_commentaire[comentaire.nbEtoiles - 1]++
-	}
-	// Légende qui s'affichera sur la gauche à côté de la barre horizontale
-	const labels = ["etoile-5", "etoile-4", "etoile-3", "etoile-2", "etoile-1"];
-	const data = {
-		labels: labels,
-		datasets: [{
-			label: "commentaires par etoiles",
-			data: nb_commentaire.reverse(),
-			backgroundColor: "rgba(255, 230, 0, 1)"
-		}]
-	};
-	const config = {
-		type: "bar",
-		data: data,
-		options: {
-			indexAxis: "y",
-		},
-	};
-	new Chart(
-		document.querySelector("#myChart"),
-		config
-	)
+    const avis = await fetch("https://sitelesbonnespieces.onrender.com/avis").then(avis => avis.json())
+    const nb_commentaire = [0, 0, 0, 0, 0]
+    for (let comentaire of avis) {
+        nb_commentaire[comentaire.nbEtoiles - 1]++
+    }
+    // Légende qui s'affichera sur la gauche à côté de la barre horizontale
+    const labels = ["etoile-5", "etoile-4", "etoile-3", "etoile-2", "etoile-1"];
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'commentaires par etoiles',
+            data: nb_commentaire.reverse(),
+            backgroundColor: "rgba(255, 230, 0, 1)"
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            indexAxis: 'y',
+        },
+    };
+    new Chart(
+        document.querySelector("#myChart"),
+        config
+    )
 
 
-	let getPieces = window.localStorage.getItem("pieces")
-	const pieces = JSON.parse(getPieces)
-	let commentaireDispo = 0
-	let commentaireNonDispo = 0
-	for (let i = 0; i < avis.length; i++) {
-		const piece = pieces.find(p => p.id === avis[i].pieceId)
-		if (piece) {
-			if (piece.disponibilite) {
-				commentaireDispo++
-			} else {
-				commentaireNonDispo++
-			}
-		}
-	}
+    let getPieces = window.localStorage.getItem("pieces")
+    const pieces = JSON.parse(getPieces)
+    let commentaireDispo = 0
+    let commentaireNonDispo = 0
+    for (let i = 0; i < avis.length; i++) {
+        const piece = pieces.find(p => p.id === avis[i].pieceId)
+        if (piece) {
+            if (piece.disponibilite) {
+                commentaireDispo++
+            } else {
+                commentaireNonDispo++
+            }
+        }
+    }
 
-	const labelsdipo = ["PiecesDispo", "PiecesNonDispo"]
-	const datapieces = {
-		labels: labelsdipo,
-		datasets: [
-			{
-				label: "Nombre de commentaire",
-				data: [commentaireDispo, commentaireNonDispo],
-				backgroundColor: " rgb(131, 132, 226)"
-			}
-		]
-	}
+    const labelsdipo = ["PiecesDispo", "PiecesNonDispo"]
+    const datapieces = {
+        labels: labelsdipo,
+        datasets: [
+            {
+                label: "Nombre de commentaire",
+                data: [commentaireDispo, commentaireNonDispo],
+                backgroundColor: " rgb(131, 132, 226)"
+            }
+        ]
+    }
 
-	const configData = {
-		type: "bar",
-		data: datapieces
-	}
+    const configData = {
+        type: "bar",
+        data: datapieces
+    }
 
-	new Chart(document.querySelector("#myChart1"), configData)
+    new Chart(document.querySelector("#myChart1"), configData)
 }
 
 
